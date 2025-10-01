8b9ca04d5423e7fe9756eb1eff1717c6a6c18678
ivan <Iwan.kudashow@gmail.com>
2025-10-01 19:30:07 +0300

    edit 13. add Avatars class, repository, service, controller

---
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -0,0 +1,79 @@
+package ru.hogwarts.school.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.dto.MapperModel;
+import ru.hogwarts.school.dto.StudentDTO;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.repository.AvatarRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+@Service
+@Transactional
+public class AvatarService {
+    @Value("${path.to.avatars.folder}")
+    private String avatarsDir;
+
+    private final AvatarRepository avatarRepository;
+    private StudentService studentService;
+
+    private MapperModel mapperModel;
+
+    public AvatarService(AvatarRepository avatarRepository, StudentService studentService) {
+        this.avatarRepository = avatarRepository;
+        this.studentService = studentService;
+    }
+
+    private String getExtensions(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+    public Avatar findAvatar(Long studentId) {
+        return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
+    }
+
+    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        StudentDTO student = studentService.readStudent(studentId);
+        Path filePath = Path.of(avatarsDir, student.getName() + "_" + student.getFacultyName() + "." + getExtensions(avatarFile.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try (
+                InputStream is = avatarFile.getInputStream();
+                OutputStream os = Files.newOutputStream(filePath, StandardOpenOption.CREATE_NEW);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
+        ) {
+            bis.transferTo(bos);
+        }
+        Avatar avatar = findAvatar(studentId);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(avatarFile.getBytes());
+        avatar.setStudent(MapperModel.toStudent(student));
+        avatarRepository.save(avatar);
+    }
+
+    public void downloadAvatar(Long studentId, HttpServletResponse response) throws IOException {
+        Avatar avatar = findAvatar(studentId);
+        Path path = Path.of(avatar.getFilePath());
+        try (
+                InputStream is = Files.newInputStream(path);
+                OutputStream os = response.getOutputStream();
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
+        ) {
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            bis.transferTo(bos);
+        }
+    }
+}
